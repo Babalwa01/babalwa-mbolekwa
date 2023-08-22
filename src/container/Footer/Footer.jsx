ea7228474be12be8d990e602e45820f98be4eab2
Babalwa01 <babalwambolekwa93@gmail.com>
2023-08-22 10:13:39 +0200

    added social media to small devices

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsTwitter, BsLinkedin, BsGithub } from "react-icons/bs";
 import { images } from "../../constants";
 import { AppWrap, MotionWrap } from "../../wrapper";
 import "./Footer.scss";
@@ -8,16 +9,39 @@ const Footer = () => {
     <>
       <h2 className="head-text">Get In Touch</h2>
       <div className="app__footer-cards">
+        <div className="social-media">
+          <div>
+            <a
+              href="https://twitter.com/_Babalwa"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsTwitter />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/babalwa-mbolekwa-a16a94147/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsLinkedin />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://github.com/Babalwa01"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsGithub />
+            </a>
+          </div>
+        </div>
         <div className="app__footer-card">
           <img src={images.email} alt="email" />
           <a href="mailto:babalwambolekwa93@gmail.com" className="p-text">
             babalwambolekwa93@gmail.com
-          </a>
-        </div>
-        <div className="app__footer-card">
-          <img src={images.mobile} alt="mobile" />
-          <a href="tel: +27 (000) 000 0000" className="p-text">
-            +27(0) 00 000 0000
           </a>
         </div>
       </div>
